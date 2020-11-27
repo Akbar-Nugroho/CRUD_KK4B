9cb513170e1f775d759e0e5ca6dbdf14193c99ed
Akbar-Nugroho <akbarnugroho12345@gmail.com>
2020-11-27 21:27:43 +0700

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('BASEURL', 'http://localhost/Crud_KK4B/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'kk4b_crud');
---
+++ app/views/home/index.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="jumbotron mt-4">
+      <h1 class="display-4">Selamat Datang di Website Pendataan Mahasiswa Saya!</h1>
+      <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
+      <hr class="my-4">
+      
+    </div>
+
+
+</div>
